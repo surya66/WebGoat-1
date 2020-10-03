623ab83087d0599fbd42bb5825c0b28f78843dc2
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-03 07:52:57 +0530

    Update install_java.sh

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -2,5 +2,5 @@
 # sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum -y install docker
 sudo service docker restart
-echo 'export WEBGOAT_HOST=$(ip -f inet -o addr show en0 | awk '{print $4}' | cut -d '/' -f 1)' >> ~/.bashrc
+echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)" >> ~/.bashrc
 source ~/.bashrc
