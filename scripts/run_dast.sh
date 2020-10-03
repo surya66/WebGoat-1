cc9968b22854537036d4ccc5d97967f5074485be
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-03 10:51:27 +0530

    Update run_dast.sh

--- scripts/run_dast.sh
+++ scripts/run_dast.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sleep 2m
 cd /tmp
-sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)/WebGoat/login -g gen.conf -x zap_testreport.xml >/dev/null 2>&1 &
+# sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)/WebGoat/login -g gen.conf -x zap_testreport.xml >/dev/null 2>&1 &
+
+sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)/WebGoat/login -g gen.conf -x zap_testreport.xml >/tmp/zap.log
