a73f89920b622152a11177eb9c776edd09f96a3a
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-04 12:16:26 +0530

    Update install_java.sh

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -2,8 +2,3 @@
 sudo yum install java -y
 sudo yum -y install docker
 sudo service docker restart
-echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)" >> /home/ec2-user/.bashrc
-source /home/ec2-user/.bashrc
-echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)" >> ~/.bashrc
-source ~/.bashrc
-# kill $(lsof -ti :80) >/dev/null 2>&1 &
