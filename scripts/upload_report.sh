5f26b5e790fd4c9ae465a021e17ca14ffc50ec1e
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-04 12:13:10 +0530

    Create upload_report.sh

---
+++ scripts/upload_report.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+su ec2-user
+aws s3 cp s3://awscsp1337/dd_upload_script.zip .
+unzip -o dd_upload_script.zip && mv dd_upload_script/* .
+pip install -r requirements.txt
+python dd_upload.py
