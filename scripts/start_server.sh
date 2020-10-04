c8b9f1fb3ee0a4144de51878ae9deecd781c2051
tiwratnesh <72146960+tiwratnesh@users.noreply.github.com>
2020-10-04 13:02:51 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@ echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cu
 source /home/ec2-user/.bashrc
 echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)" >> ~/.bashrc
 source ~/.bashrc
-java -jar /tmp/webgoat-server-v8.1.0.jar --server.port=80 >/dev/null 2>&1 &
+java -jar webgoat-server-v8.1.0.jar --server.port=80 >/dev/null 2>&1 &
