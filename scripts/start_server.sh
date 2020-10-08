8744e57c164af0dbe6fecf8503ea01f33ed537c0
Ratnesh Tiwari <ratnesht@vmware.com>
2020-10-08 07:14:07 +0530

    test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,5 @@ echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cu
 source /home/ec2-user/.bashrc
 echo "export WEBGOAT_HOST=$(ip -f inet -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1)" >> ~/.bashrc
 source ~/.bashrc
+# kill $(sudo lsof -t -i:80)
 java -jar webgoat-server-v8.1.0.jar --server.port=80 >/dev/null 2>&1 &
