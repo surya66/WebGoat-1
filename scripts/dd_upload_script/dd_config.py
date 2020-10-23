030ba523571c5ea482cc9aa81f16ba6cd302753b
Surya Mettupalli <surya66@gmail.com>
2020-10-23 09:56:49 -0400

    Update dd_config.py

--- scripts/dd_upload_script/dd_config.py
+++ scripts/dd_upload_script/dd_config.py
@@ -10,7 +10,7 @@ __author__ = 'ratnesht'
 # ------------------------- DD UPLOAD SCAN INFO ------------------------------------------------------------------------
 
 DEFECT_DOJO_HOST = 'http://127.0.0.1:8080'
-DEFECT_DOJO_API_KEY = '3d352a15d9a1a97b66812ba98433347c67b8d8b2'
+DEFECT_DOJO_API_KEY = 'ee8f73803f55db3375c82ed48a7eb810e86faa14'
 DEFECT_DOJO_USERNAME = 'admin'
 
 SCANNER = ['Dependency Check Scan', 'Snyk Scan', 'ZAP Scan']
